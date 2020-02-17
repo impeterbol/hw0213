e6cb9f71e7212660f65d4ae51fe43590ba055466
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-02-17 12:40:10 -0800

    added functionality where all the info is parsed from GitHub and saved into html page. to add - convertion into pdf and more fields + styling

--- index.js
+++ index.js
@@ -1,17 +1,90 @@
 const fs = require('fs');
-    const axios = require('axios');
-    const inquirer = require('inquirer');
+const axios = require('axios');
+const inquirer = require('inquirer');
+const generateHTML = require('./generateHTML')
 
-inquirer
-.prompt({
-    type:'input',
-    message: 'Your name',
-    name: 'name'
-},
-{
-    type:'input',
-    message: 'Your name',
-    name: 'name'
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(fileName, data, err => {
+            if (err) reject(err)
+            resolve("Made a file")
+        })
+    })
 }
 
-)
+async function initES6() {
+    try {
+        const { username, color } = await inquirer.prompt([
+            {
+                message: 'Enter your Github username',
+                name: 'username'
+            },
+            {
+                name: 'color',
+                type: 'list',
+                choices: ['green', 'blue', 'pink', 'red']
+            }
+        ])
+        if (!username) {
+            throw "No User!"
+        }
+
+        const github = await axios.get(`https://api.github.com/users/${username}`);
+        const repos = await axios.get(`https://api.github.com/users/${username}/repos?per_page=100`);
+        const repoNames = repos.data.map(repo => repo.name);
+
+        // location: github.location
+        // name: github.name
+        // company: github.company
+        console.log(github)
+        const html = generateHTML({
+            color,
+            ...github.data
+        })
+        console.log(html)
+        await writeToFile('./profile.html', html);
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function init() {
+    inquirer
+        .prompt({
+            message: 'Enter your Github username',
+            name: 'username'
+        })
+        .then(({ username }) => {
+            const url = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+            axios.get(url)
+                .then((res) => {
+                    const repoNames = res.data.map(repo => repo.name);
+
+                    writeToFile('./index-test.js', repoNames)
+                        .then(function (res) {
+                            console.log(res)
+                        })
+                        .catch(function (err) {
+                            console.log(err)
+                        })
+                })
+                .catch(function (err) {
+                    console.log(err)
+                })
+        })
+        .catch(function (err) {
+            console.log(err)
+        });
+}
+
+initES6();
+
+
+
